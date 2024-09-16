46ad0fc7810e09dbfca21e79da0f84d3bd521078
EmeBer91 <emeber987@ihmeducation.se>
2024-09-16 09:29:51 +0200

    java

---
+++ js/app.js
@@ -0,0 +1,12 @@
+console.log("This is line 1.");
+console.log("This is line 2.");
+console.log("This is line 3.");
+console.log("This is line 4.");
+console.log("This is line 5.");
+console.log("This is line 6.");
+
+// Author: Emelie
+// Date: 2024-09-15
+// Email: dittnamn@example.com
+
+console.log("Hello World!");
